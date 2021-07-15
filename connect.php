7bfce7691f6a0bb96ba536cfb16483f43f11a417
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-07-16 03:46:56 +0600

    added connect php

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$name= "aporbo";
+ echo $name;
+
+ $number1 = 20;
+ $number2 = 10;
+
+$number3 = $number1/$number2;
+echo $number3;
+
+$a[0] = 50;
+$a[1] = 50;
+$a[2] = 50;
+$a[] = 50;
+
+print_r($a);
