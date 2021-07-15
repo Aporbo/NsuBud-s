b9b4c5b87154c5b4919d8ac6019a1b7714a9d879
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-07-15 22:20:48 +0600

    added signup page with css

---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NSUBUD's | Signup</title>
+    <link rel="stylesheet" href="signup.css">
+</head>
+<body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color:#e9ebee;">
+   <div id="bar">
+       <div style="font-size:40px">
+       NSUBUD's 
+       </div> 
+       <div id="signup_button" >
+       Log in
+       </div>
+      
+   </div>
+
+   <div id="bar2">
+         Sign up to NSUBUD's <br><br>
+         <input type="text" placeholder="First Name" id="text"><br><br>
+
+         <input type="text" placeholder="Last Name" id="text"><br><br>
+
+       <span style="font-weight: normal;"> Gender: </span> <br>
+         <select id="text">
+             <option >Male</option>
+             <option >Female</option>
+
+         </select> <br><br>
+
+         <input type="text" placeholder="Email" id="text"><br><br>
+         <input type="password" placeholder="Password" id="text"><br><br>
+         <input type="password" placeholder="Re-Type Password" id="text"><br><br>
+
+         <input type="submit" id="button" value="Sign up" ><br><br><br>
+   </div>
+
+</body>
+</html>
