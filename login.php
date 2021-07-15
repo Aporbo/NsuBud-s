2fb51d57a0d09d014e033e8d192cf3bae17271bf
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-07-15 21:18:49 +0600

    added login page without css

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NSUBUD's</title>
+</head>
+<style>
+#bar{
+      height:100px; 
+      background-color:#3c5a99;
+      color:#d9dfeb;  
+      padding: 4px;   
+}
+#signup_button{
+    background-color: #42b72a;
+    width: 70px;
+    text-align: center;
+    padding: 4px;
+    border-radius: 4px;
+    float: right;
+
+}
+#bar2{
+ background-color:white;
+  width:800px; 
+  height:400px;
+  margin:auto;
+  margin-top:50px;
+  padding: 10px;
+  padding-top: 50px;
+  text-align: center; 
+}
+</style>
+<body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color:#e9ebee;">
+   <div id="bar">
+       <div style="font-size:40px">
+       NSUBUD's 
+       </div> 
+       <div id="signup_button" >
+       Signup
+       </div>
+      
+   </div>
+
+   <div id="bar2">
+         Login to NSUBUD's <br><br>
+         <input type="text" id="text"><br><br>
+         <input type="text" id="text"><br><br>
+         <input type="submit" id="button" value="Login" >
+   </div>
+
+</body>
+</html>
