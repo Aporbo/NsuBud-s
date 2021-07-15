44df8646041e0cd70719f2454d4e7260cad68827
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-07-15 21:38:00 +0600

    css added

--- login.php
+++ login.php
@@ -4,35 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NSUBUD's</title>
+    <title>NSUBUD's | Login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
-<style>
-#bar{
-      height:100px; 
-      background-color:#3c5a99;
-      color:#d9dfeb;  
-      padding: 4px;   
-}
-#signup_button{
-    background-color: #42b72a;
-    width: 70px;
-    text-align: center;
-    padding: 4px;
-    border-radius: 4px;
-    float: right;
-
-}
-#bar2{
- background-color:white;
-  width:800px; 
-  height:400px;
-  margin:auto;
-  margin-top:50px;
-  padding: 10px;
-  padding-top: 50px;
-  text-align: center; 
-}
-</style>
 <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color:#e9ebee;">
    <div id="bar">
        <div style="font-size:40px">
@@ -46,9 +20,9 @@
 
    <div id="bar2">
          Login to NSUBUD's <br><br>
-         <input type="text" id="text"><br><br>
-         <input type="text" id="text"><br><br>
-         <input type="submit" id="button" value="Login" >
+         <input type="text" placeholder="Email" id="text"><br><br>
+         <input type="password" placeholder="Password" id="text"><br><br>
+         <input type="submit" id="button" value="Login" ><br><br><br>
    </div>
 
 </body>
