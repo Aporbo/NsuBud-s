03be242fa18e7a8ad87a93aa62d93be722bed4eb
Aporbo <aporbogosh249@gmail.com>
2021-08-28 20:34:09 +0600

    added forget password all pages

---
+++ Frontend and UI/Forgot_Password_background.php
@@ -0,0 +1,33 @@
+<?php
+	include("Login.php");
+?>
+<html>
+<head>
+<LINK REL="SHORTCUT ICON" HREF="nsubud_files/nsubud_title_icon/Faceback.ico" />
+	<style>
+		#singup_button
+		{
+			background:#e21c6f
+;
+			color:#FFFFFF;
+			border-top-color:#3B6E22;
+			border-right-color:#2C5115;
+			border-left-color:#3B6E22;
+			font-size:15px;
+			height:30;
+			width:75;
+			font-weight:bold;
+			box-shadow:-5px 0px 10px 1px rgb(0,0,0);
+		}
+	</style>
+     <link href="nsubud_files/nsubud_font/font.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div style="position:absolute;left:0%;top:0%; height:13.2%; width:100%; z-index:-1; background:#d91835">   </div>
+<div style="position:absolute;left:13.5%; top:3.3%; font-size:45; font-weight:900; color:#FFFFFF; font-weight:bold;"> <font face="mynsubudFont">NSUBUD's </font> </div>
+
+<div style="position:absolute;left:13.6%; top:14.8%;"> <a href="index.php" style="text-decoration:none;"> <input type="button" value="Sign Up" id="singup_button">    </a> </div>
+
+</body>
+</html>
