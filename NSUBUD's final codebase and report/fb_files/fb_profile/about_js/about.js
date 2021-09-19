a1fbe9281a6e8ada45c3e54ad7f420d34499f84b
Aporbo <aporbogosh249@gmail.com>
2021-09-20 01:48:16 +0600

    added logout and profile

---
+++ NSUBUD's final codebase and report/fb_files/fb_profile/about_js/about.js
@@ -0,0 +1,57 @@
+function work_static_hide()
+{
+	document.getElementById("work_static").style.display='none';
+	document.getElementById("Work_form").style.display='block';
+}
+function Living_static_hide()
+{
+	document.getElementById("Living_static").style.display='none';
+	document.getElementById("Living_form").style.display='block';
+}
+function work_form_hide()
+{
+	document.getElementById("Work_form").style.display='none';
+	document.getElementById("work_static").style.display='block';
+}
+function living_form_hide()
+{
+	document.getElementById("Living_form").style.display='none';
+	document.getElementById("Living_static").style.display='block';
+}
+function basic_static_hide()
+{
+	document.getElementById("basic_static").style.display='none';
+	document.getElementById("basic_form").style.display='block';
+}
+function basic_form_hide()
+{
+	document.getElementById("basic_form").style.display='none';
+	document.getElementById("basic_static").style.display='block';
+}
+function contact_static_hide()
+{
+	document.getElementById("contact_static").style.display='none';
+	document.getElementById("contact_form").style.display='block';
+}
+function contact_form_hide()
+{
+	document.getElementById("mobile_no_erorr").style.display='none';
+	document.getElementById("contact_form").style.display='none';
+	document.getElementById("contact_static").style.display='block';
+}
+		
+function contact_check()
+{
+	
+}
+
+function isNumberKey(evt)
+{
+
+}
+
+
+function isStringKey(evt)
+{
+	
+}
