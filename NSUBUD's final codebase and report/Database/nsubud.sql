91045cb4dea15dbd215a201aba02d0d81f5244d6
Ayesha Rahman <ayesharahman000300@gmail.com>
2021-09-20 01:20:16 +0600

    added database & fb files

---
+++ NSUBUD's final codebase and report/Database/nsubud.sql
@@ -0,0 +1,249 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+CREATE TABLE IF NOT EXISTS `admin_info` (
+  `Username` varchar(200) NOT NULL,
+  `Password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `admin_info` (`Username`, `Password`) VALUES
+('98d34c1758b15b5a359b69c2b08c5767', '98d34c1758b15b5a359b69c2b08c5767');
+
+
+CREATE TABLE IF NOT EXISTS `help` (
+  `help_id` int(7) NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `help_txt` varchar(400) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `Date` varchar(30) NOT NULL,
+  PRIMARY KEY (`help_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+
+
+INSERT INTO `help` (`help_id`, `user_id`, `help_txt`, `type`, `Date`) VALUES
+(2, 8, 'nice', 'password', '30-9-2021 11:34');
+
+
+CREATE TABLE IF NOT EXISTS `group_chat` (
+  `chat_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `chat_txt` text NOT NULL,
+  `time` varchar(30) NOT NULL,
+  PRIMARY KEY (`chat_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+
+
+INSERT INTO `group_chat` (`chat_id`, `user_id`, `chat_txt`, `time`) VALUES
+(1, 8, 'hello ', '10-9-2021 11:35');
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(7) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(25) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `Gender` varchar(6) NOT NULL,
+  `Birthday_Date` varchar(11) NOT NULL,
+  `FB_Join_Date` varchar(30) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+
+
+
+INSERT INTO `users` (`user_id`, `Name`, `Email`, `Password`, `Gender`, `Birthday_Date`, `FB_Join_Date`) VALUES
+(0, 'Aporbo Ghosh', 'aporbo@northsouth.edu', 'aporbo1121999', 'Male', '01-12-1999', '10-9-21 22:10');
+
+
+
+CREATE TABLE IF NOT EXISTS `users_notice` (
+  `notice_id` int(7) NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `notice_txt` varchar(120) NOT NULL,
+  `notice_time` varchar(30) NOT NULL,
+  PRIMARY KEY (`notice_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `user_cover_pic` (
+  `cover_id` int(7) NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `image` varchar(150) NOT NULL,
+  PRIMARY KEY (`cover_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+
+
+
+INSERT INTO `user_cover_pic` (`cover_id`, `user_id`, `image`) VALUES
+(7, 8, '');
+
+
+CREATE TABLE IF NOT EXISTS `user_info` (
+  `user_id` int(7) NOT NULL,
+  `job` varchar(100) NOT NULL,
+  `school_or_collage` varchar(100) NOT NULL,
+  `current_city` varchar(100) NOT NULL,
+  `hometown` varchar(100) NOT NULL,
+  `relationship_status` varchar(30) NOT NULL,
+  `mobile_no` varchar(15) NOT NULL,
+  `mobile_no_priority` int(11) NOT NULL,
+  `website` varchar(100) NOT NULL,
+  `Facebook_ID` varchar(100) NOT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `user_info` (`user_id`, `job`, `school_or_collage`, `current_city`, `hometown`, `relationship_status`, `mobile_no`, `mobile_no_priority`, `website`, `Facebook_ID`) VALUES
+(0, '', '', '', '', '', '', '', '', '');
+
+
+CREATE TABLE IF NOT EXISTS `user_post` (
+  `post_id` int(7) NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `post_txt` text NOT NULL,
+  `post_pic` varchar(150) NOT NULL,
+  `post_time` varchar(30) NOT NULL,
+  `priority` varchar(8) NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=79 ;
+
+
+
+INSERT INTO `user_post` (`post_id`, `user_id`, `post_txt`, `post_pic`, `post_time`, `priority`) VALUES
+(46, 8, 'Join nsubud', '', '10-9-2021 22:10', 'Public');
+
+
+
+CREATE TABLE IF NOT EXISTS `user_post_comment` (
+  `comment_id` int(7) NOT NULL AUTO_INCREMENT,
+  `post_id` int(7) NOT NULL,
+  `user_id` int(7) NOT NULL,
+  `comment` text NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `user_id` (`user_id`),
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+
+CREATE TABLE IF NOT EXISTS `user_post_status` (
+  `status_id` int(7) NOT NULL AUTO_INCREMENT,
+  `post_id` int(7) NOT NULL,
+  `user_id` int(7) NOT NULL,
+  `status` varchar(7) NOT NULL,
+  PRIMARY KEY (`status_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+
+
+
+CREATE TABLE IF NOT EXISTS `user_profile_pic` (
+  `profile_id` int(7) NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `image` varchar(150) NOT NULL,
+  PRIMARY KEY (`profile_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+
+
+INSERT INTO `user_profile_pic` (`profile_id`, `user_id`, `image`) VALUES
+(6, 8, '');
+
+
+
+CREATE TABLE IF NOT EXISTS `user_secret_quotes` (
+  `user_id` int(7) NOT NULL,
+  `Question1` varchar(50) NOT NULL,
+  `Answer1` varchar(20) NOT NULL,
+  `Question2` varchar(50) NOT NULL,
+  `Answer2` varchar(20) NOT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `user_secret_quotes` (`user_id`, `Question1`, `Answer1`, `Question2`, `Answer2`) VALUES
+(8, '', '', '', '');
+
+
+
+CREATE TABLE IF NOT EXISTS `user_status` (
+  `user_id` int(7) NOT NULL,
+  `status` varchar(8) NOT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `user_status` (`user_id`, `status`) VALUES
+(8, 'Offline');
+
+
+
+CREATE TABLE IF NOT EXISTS `user_warning` (
+  `user_id` int(7) NOT NULL,
+  `warning_txt` varchar(200) NOT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `help`
+  ADD CONSTRAINT `help_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `group_chat`
+  ADD CONSTRAINT `group_chat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `users_notice`
+  ADD CONSTRAINT `users_notice_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_cover_pic`
+  ADD CONSTRAINT `user_cover_pic_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_info`
+  ADD CONSTRAINT `user_info_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_post`
+  ADD CONSTRAINT `user_post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_post_comment`
+  ADD CONSTRAINT `user_post_comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_post_comment_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `user_post` (`post_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_post_status`
+  ADD CONSTRAINT `user_post_status_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_post_status_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user_post` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_profile_pic`
+  ADD CONSTRAINT `user_profile_pic_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_secret_quotes`
+  ADD CONSTRAINT `user_secret_quotes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_status`
+  ADD CONSTRAINT `user_status_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_status_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_warning`
+ 
---
+++ NSUBUD's final codebase and report/fb_files/fb_home/help.php
@@ -0,0 +1,196 @@
+<?php
+	session_start();
+	error_reporting(1);
+	if(isset($_SESSION['fbuser']))
+	{
+			$con= mysqli_connect("localhost","root","");
+		mysqli_select_db($con,"nsubud");
+		$user_email=$_SESSION['fbuser'];
+		$que_user_info=mysqli_query($con,"select * from users where Email='$user_email'");
+		$user_data=mysqli_fetch_array($que_user_info);
+		$userid=$user_data[0];
+		
+		if(isset($_POST['help']))
+		{
+			$fb_txt=$_POST['help_txt'];
+			$type=$_POST['type'];
+			$fb_time=$_POST['help_time'];
+			mysqli_query($con,"insert into help(user_id,help_txt,type,Date) values($userid,'$fb_txt','$type','$fb_time')");
+		}
+		
+		
+		if(isset($_POST['delete_help']))
+		{
+			$fb_id=intval($_POST['help_id']);
+			mysqli_query($con,"delete from help where help_id=$fb_id");
+		}
+		
+		
+		include("background.php");
+?>
+<html>
+<head>
+<title> Help </title>
+<style>
+#help_button
+{
+	font-size:14px;
+	height:30;
+	width:80;
+	padding:2;
+	background-color:#e21c6f; color:#FFFFFF;
+	border-top:#29447E;
+	border-right-color:#29447E;
+	border-bottom-color:#1A356E;
+	border-left-color:#29447E;
+	font-weight:bold;
+}
+</style>
+<script>
+	function blank_help_check()
+	{
+		var help_txt=document.help_form.help_txt.value;
+		if(help_txt=="")
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	function help_name_underLine(fid)
+	{
+		document.getElementById("uname"+fid).style.textDecoration = "underline";
+	}
+	function help_name_NounderLine(fid)
+	{
+		document.getElementById("uname"+fid).style.textDecoration = "none"
+	}
+	
+	function time_get()
+	{
+			d = new Date();
+			mon = d.getMonth()+1;
+			time = d.getDate()+"-"+mon+"-"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes();
+			help_form.help_time.value=time;
+	}
+</script>
+</head>
+<body>
+	
+	<div style="position:absolute; left:25%; top:10%;"> <img style="height:95px; " src="img/help_icon.png"> </div>
+	<div style="position:absolute; left:35%; top:12%;"> <h1 style="color:#3B59A4;"> Help </h1> </div>
+    <hr style="position:absolute;left:25%;top:25%;height:0.5%;width:50%; border-color:#e21c6f; box-shadow:0px 5px 5px 0px rgb(0,0,0); ">
+    
+    <div style=" background:#00122e
+; position:absolute; left:25%; top:32%; height:24%; width:41.4%; z-index:-1; box-shadow:0px 2px 10px 1px rgb(0,0,0);"> </div>
+    
+    <form method="post" name="help_form" onSubmit="return blank_help_check()">
+	
+	<div style="position:absolute; left:25.3%; top:32.5%;">
+		<textarea style="height:100; width:550;" name="help_txt" maxlength="100" placeholder="Write your problem and send us"></textarea>
+	</div>	
+    <div style="position:absolute; left:26%; top:48.4%;"> <img style="height:40px; weight:100%" src="img/star.png"> </div>
+    <div style="position:absolute; left:30%; top:50.5%;">
+    	<select name="type" style=" font-size:16px; height:25; width:90;"> 
+			<option value="Account"> Account </option> 
+			<option value="Password"> Password </option> 
+            <option value="Login"> Login </option> 
+			<option value="Message"> Message </option>
+            <option value="signup"> signup </option> 
+		</select> 
+    </div>
+    <input type="hidden" name="help_time">
+    <div style="position:absolute; left:59%; top:50%;"> <input type="submit" value="Help" name="help" id="help_button" onClick="time_get()"> </div>
+    </form>
+    
+<?php
+		$que_help=mysqli_query($con,"select * from help order by help_id desc");
+?>
+    <div style="position:absolute; left:20%; top:63%;">
+    <table border="0">
+<?php
+	while($help_data=mysqli_fetch_array($que_help))
+	{
+		$help_id=$help_data[0];
+		$fb_user_id=$help_data[1];
+		$fb_txt=$help_data[2];
+		$fb_txt2=$help_data[5];
+		$fb_type=$help_data[3];
+		$fb_time=$help_data[4];
+		$que_fb_user_info=mysqli_query($con,"select * from users where user_id=$fb_user_id");
+		$fb_user_data=mysqli_fetch_array($que_fb_user_info);
+		$user_name=$fb_user_data[1];
+		$user_email=$fb_user_data[2];
+		$user_gender=$fb_user_data[4];
+		$que_fb_user_pic=mysqli_query($con,"select * from user_profile_pic where user_id=$fb_user_id");
+		$fetch_user_pic=mysqli_fetch_array($que_fb_user_pic);
+		$user_pic=$fetch_user_pic[2];
+?>
+	<tr>
+<?php    
+    if($fb_user_id==$userid)
+    {
+?>
+	<td colspan="3"align="right" style="border-top:outset; border-top-width:thin;"> 
+			<form method="post">  
+				<input type="hidden" name="help_id" value="<?php echo $help_id; ?>" >
+				<input type="submit" name="delete_help" value="Delete" style="background-color:#FFFFFF; border;  "> 
+			</form>
+     </td>
+     <td>  </td>
+<?php
+	}
+	else
+	{
+?>
+	<td colspan="3"align="right" style="border-top:outset; border-top-width:thin;">&nbsp;  </td>
+     <td>  </td>
+     
+			
+<?php
+	}
+?>
+			
+     </tr>
+    
+	<tr>
+    	<td style="padding-left:25;" rowspan="2"> <img src="../../fb_users/<?php echo $user_gender; ?>/<?php echo $user_email; ?>/Profile/<?php echo $user_pic; ?>" height="60" width="55">  </td>
+        <td colspan="2" style="padding:7;"> <a href="../fb_view_profile/view_profile.php?id=<?php echo $fb_user_id; ?>" style="text-transform:capitalize; text-decoration:none; color:#003399;" onMouseOver="help_name_underLine(<?php echo $help_id; ?>)" onMouseOut="help_name_NounderLine(<?php echo $help_id; ?>)" id="uname<?php echo $help_id; ?>"> <?php echo $user_name; ?> </a>   </td>
+       
+    </tr>
+    <tr>
+		<td colspan="2" style=" padding-left:15; "><?php echo $fb_txt; ?></td>
+        <td> </td>
+        <td> </td>
+	</tr>
+    <tr>
+		
+	</tr>
+	<tr>
+		<td colspan="2" style=" padding-left:10;"><?php echo $fb_txt2; ?></td>
+        <td> </td>
+        <td> </td>
+		
+	</tr>
+    	<td> </td>
+        <td style=" padding-left:7;"> <span style="color:#999999;">  <?php echo $fb_type; ?>  </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style="color:#999999;"> <?php echo $fb_time; ?> </span> </td>
+        <td> </td>
+    </tr>
+    <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+<?php
+	}
+	
+?>
+   </table></div>
+   
+   <div style="position:absolute; left:90%; top:100%;" > &nbsp; </div>	
+
+</body>
+</html>
+<?php
+	}
+	else
+	{
+		header("location:../../index.php");
+	}
+?>
